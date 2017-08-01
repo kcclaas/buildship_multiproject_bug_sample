5ee2e5ea1f52ea09e81dd9ced2933da07dd57ae0
Kevin Claas <kclaas@ordinalscience.com>
2017-08-01 03:35:55 +0000

    initial commit

---
+++ ProjectA/src/test/kcclaas/hello/Hello.java
@@ -0,0 +1,9 @@
+package test.kcclaas.hello;
+
+import test.kcclaas.world.World;
+
+public class Hello {
+    public static void main(String... args) {
+        System.out.println("Hello " + World.getString());
+    }
+}
---
+++ ProjectB/src/test/kcclaas/world/World.java
@@ -0,0 +1,7 @@
+package test.kcclaas.world;
+
+public class World {
+    public static String getString() {
+        return "World";
+    }
+}
