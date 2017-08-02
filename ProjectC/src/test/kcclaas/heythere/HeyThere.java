fbd218b5e55002fab6cb70ddfcbf69e4502e6e20
Kevin Claas <kclaas@ordinalscience.com>
2017-08-02 18:49:12 +0000

    added a third project, also dependent on ProjectB

---
+++ ProjectC/src/test/kcclaas/heythere/HeyThere.java
@@ -0,0 +1,9 @@
+package test.kcclaas.heythere;
+
+import test.kcclaas.world.World;
+
+public class HeyThere {
+    public static void main(String... args) {
+        System.out.println("Hey there, " + World.getString());
+    }
+}
